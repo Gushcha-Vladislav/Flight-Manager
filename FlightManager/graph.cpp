558e57f556537b6dbeefb1fe6d8ed40034488cfa
daria.laptsevich <daria.laptsevich@team.wrike.com>
2019-05-19 19:01:47 +0300

    decstra algorithm

--- FlightManager/graph.cpp
+++ FlightManager/graph.cpp
@@ -11,103 +11,6 @@ Graph::Graph(std::vector<Vertex> *vertes)
     this->vertexs = vertes;
 }
 
-<<<<<<< HEAD
-std::string Graph::find_way(int from_id, int to_id)
-{
-
-//    Vertex v1 = new Vertex('Moscow', 10, 10, );
-    int SIZE = this->vertexs->size();
-    int a[SIZE][SIZE]; // матрица связей
-    int d[SIZE]; // минимальное расстояние
-    int v[SIZE]; // посещенные вершины
-    int temp;
-    int minindex, min;
-
-    // Инициализация матрицы связей
-    for (int i = 0; i<SIZE; i++)
-    {
-      a[i][i] = 0;
-
-      for (int j = i + 1; j<SIZE; j++) {
-          int temp_time = 0;
-          std::vector<Edge>* edges = this->vertexs->at(i).get_edges();
-          int to_vert_id = this->vertexs->at(j).id;
-          foreach (Edge edge, edges) {
-              if (edge.get_to_id() == to_vert_id) temp_time = edge.get_fly_time();
-          }
-          a[i][j] = temp;
-          a[j][i] = temp;
-      }
-    }
-    //Инициализация вершин и расстояний
-      for (int i = 0; i<SIZE; i++)
-      {
-        d[i] = 10000;
-        v[i] = 1;
-      }
-      d[0] = 0;
-      // Шаг алгоритма
-      do {
-        minindex = 10000;
-        min = 10000;
-        for (int i = 0; i<SIZE; i++)
-        { // Если вершину ещё не обошли и вес меньше min
-          if ((v[i] == 1) && (d[i]<min))
-          { // Переприсваиваем значения
-            min = d[i];
-            minindex = i;
-          }
-        }
-        // Добавляем найденный минимальный вес
-        // к текущему весу вершины
-        // и сравниваем с текущим минимальным весом вершины
-        if (minindex != 10000)
-        {
-          for (int i = 0; i<SIZE; i++)
-          {
-            if (a[minindex][i] > 0)
-            {
-              temp = min + a[minindex][i];
-              if (temp < d[i])
-              {
-                d[i] = temp;
-              }
-            }
-          }
-          v[minindex] = 0;
-        }
-      } while (minindex < 10000);
-      // Вывод кратчайших расстояний до вершин
-      printf("\nКратчайшие расстояния до вершин: \n");
-      for (int i = 0; i<SIZE; i++)
-        printf("%5d ", d[i]);
-      // Восстановление пути
-      int ver[SIZE]; // массив посещенных вершин
-      int end = 4; // индекс конечной вершины = 5 - 1
-      ver[0] = end + 1; // начальный элемент - конечная вершина
-      int k = 1; // индекс предыдущей вершины
-      int weight = d[end]; // вес конечной вершины
-
-      while (end > 0) // пока не дошли до начальной вершины
-      {
-        for(int i=0; i<SIZE; i++) // просматриваем все вершины
-          if (a[end][i] != 0)   // если связь есть
-          {
-            int temp = weight - a[end][i]; // определяем вес пути из предыдущей вершины
-            if (temp == d[i]) // если вес совпал с рассчитанным
-            {                 // значит из этой вершины и был переход
-              weight = temp; // сохраняем новый вес
-              end = i;       // сохраняем предыдущую вершину
-              ver[k] = i + 1; // и записываем ее в массив
-              k++;
-            }
-          }
-      }
-
-    qDebug();
-
-}
-=======
 Graph* Graph::add_vertex(Vertex vertex)
 {
     this->vertexs->push_back(vertex);
@@ -147,4 +50,120 @@ Graph::~Graph(){
     delete this->vertexs;
 }
 
->>>>>>> 6b422697b80d99b33a34c31f8508fb73367344d4
+std::string Graph::find_way(int from_id, int to_id)
+{
+    int MAX_INT = 100000;
+    int SIZE = this->vertexs->size();
+    int a[SIZE][SIZE]; // матрица связей
+    int d[SIZE]; // минимальное расстояние
+    int v[SIZE]; // посещенные вершины
+    int temp;
+    int minindex, min;
+
+    int begin_vert_index = 0;
+    int end_vert_index = 0;
+
+    foreach (Vertex vert, this->vertexs) {
+        if ((begin_vert_index != 0) && (end_vert_index != 0)) break;
+        if (vert.get_id() == from_id) begin_vert_index = vert.get_id();
+        if (vert.get_id() == to_id) end_vert_index = vert.get_id();
+    }
+
+    std::swap(this->vertexs->at(0), this->vertexs->at(begin_vert_index));
+    std::swap(this->vertexs->at(SIZE - 1), this->vertexs->at(end_vert_index));
+
+    // Инициализация матрицы связей
+    for (int i = 0; i<SIZE; i++)
+    {
+      a[i][i] = 0;
+
+      for (int j = i + 1; j<SIZE; j++) {
+          int temp_time = 0;
+          std::vector<Edge>* edges = this->vertexs->at(i).get_edges();
+          int to_vert_id = this->vertexs->at(j).get_id();
+          foreach (Edge edge, edges) {
+              if (edge.get_to_id() == to_vert_id) temp_time = edge.get_fly_time();
+          }
+          a[i][j] = temp_time;
+          a[j][i] = temp_time;
+      }
+    }
+
+    // Вывод матрицы связей
+    for (int i = 0; i<SIZE; i++)
+    {
+      for (int j = 0; j<SIZE; j++) {
+         qDebug() << a[i][j];
+         qDebug() << " ";
+      }
+      qDebug() << "\n";
+    }
+
+    //Инициализация вершин и расстояний
+      for (int i = 0; i<SIZE; i++)
+      {
+        d[i] = MAX_INT;
+        v[i] = 1;
+      }
+      d[0] = 0;
+      // Шаг алгоритма
+      do {
+        minindex = MAX_INT;
+        min = MAX_INT;
+        for (int i = 0; i<SIZE; i++)
+        { // Если вершину ещё не обошли и вес меньше min
+          if ((v[i] == 1) && (d[i]<min))
+          { // Переприсваиваем значения
+            min = d[i];
+            minindex = i;
+          }
+        }
+        // Добавляем найденный минимальный вес
+        // к текущему весу вершины
+        // и сравниваем с текущим минимальным весом вершины
+        if (minindex != MAX_INT)
+        {
+          for (int i = 0; i<SIZE; i++)
+          {
+            if (a[minindex][i] > 0)
+            {
+              temp = min + a[minindex][i];
+              if (temp < d[i])
+              {
+                d[i] = temp;
+              }
+            }
+          }
+          v[minindex] = 0;
+        }
+      } while (minindex < MAX_INT);
+      // Вывод кратчайших расстояний до вершин
+      qDebug() << "\nКратчайшие расстояния до вершин: \n";
+      for (int i = 0; i<SIZE; i++)
+        qDebug() << d[i] + " ";
+      // Восстановление пути
+      std::string ver[SIZE]; // массив посещенных вершин
+      int end = SIZE - 1; // индекс конечной вершины = 5 - 1
+      ver[0] = this->vertexs->at(end).get_name(); // начальный элемент - конечная вершина
+      int k = 1; // индекс предыдущей вершины
+      int weight = d[end]; // вес конечной вершины
+
+      while (end > 0) // пока не дошли до начальной вершины
+      {
+        for(int i=0; i<SIZE; i++) // просматриваем все вершины
+          if (a[end][i] != 0)   // если связь есть
+          {
+            int temp = weight - a[end][i]; // определяем вес пути из предыдущей вершины
+            if (temp == d[i]) // если вес совпал с рассчитанным
+            {                 // значит из этой вершины и был переход
+              weight = temp; // сохраняем новый вес
+              end = i;       // сохраняем предыдущую вершину
+              ver[k] = this->vertexs->at(i).get_name(); // и записываем ее в массив
+              k++;
+            }
+          }
+      }
+
+    qDebug();
+
+}
