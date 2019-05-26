4873900d588d668316a4b29a4b04f4dedcc6df29
uhushcha <uladzislau.hushcha@yandex.ru>
2019-05-26 16:31:42 +0300

    Merge branch 'master' of https://github.com/Gushcha-Vladislav/spitsyn-oop
    # Conflicts:
    #	FlightManager/graph.cpp
    #	FlightManager/graph.h
    #	FlightManager/iterator.h
    #	FlightManager/vertex.cpp
    #	FlightManager/vertex.h

--- FlightManager/mainwindow.cpp
+++ FlightManager/mainwindow.cpp
@@ -3,15 +3,12 @@
 #include "graph.h"
 #include "vertex.h"
 #include "edge.h"
-#include "graph.cpp"
-#include "vertex.cpp"
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     Graph<Vertex,Edge> *graph = new Graph<Vertex,Edge>();
-    graph->test();
     graph->~Graph();
 
     ui->setupUi(this);
